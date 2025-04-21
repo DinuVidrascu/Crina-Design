bf2c3a260ce003f430fea90392689e4cc3bd9370
Dinu Vidrascu <96vlas@gmail.com>
2025-04-21 21:12:25 +0300

    fix bug-2

--- src/pages/ProjectList.jsx
+++ src/pages/ProjectList.jsx
@@ -57,23 +57,7 @@ const ProjectList = () => (
         ))}
       </motion.div>
     </motion.section>
-     <motion.button
-            onClick={scrollToTop}
-            initial={{ opacity: 1 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, ease: 'easeInOut' }}
-            className="fixed bottom-8 right-8 p-4 rounded-full bg-gradient-to-r from-[#2b2b2b] to-[#3a3a3a] backdrop-blur-md text-white shadow-xl hover:bg-gradient-to-r hover:from-[#444444] hover:to-[#555555] hover:scale-110 transition-all duration-300 group"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 group-hover:rotate-180 transition-transform duration-300"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7-7-7 7" />
-            </svg>
-          </motion.button>
+    
 
     <Footer />
   </div>
